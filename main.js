97cc6af9ac25f4ee76f6583167e3a94cace17605
Blackforest321 <98971471+Blackforest321@users.noreply.github.com>
2023-01-25 16:44:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,49 @@
+Webcam.set(
+    {
+        width:350 ,
+        height:300  ,
+        image_format:'jpeg',
+        jpeg_quality:90
+    }
+);
+
+camera=document.getElementById("camera");
+
+Webcam.attach(camera);
+
+function take_snapshot()
+{
+    Webcam.snap(
+        function(data_uri)
+        {
+            document.getElementById("result").innerHTML='<img id="captured_img" src="'+data_uri+'">';
+        }
+    );
+};
+
+console.log("ml5.version",ml5.version);
+
+classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/OC71ye5D7/model.json",modelloaded);
+
+function modelloaded()
+{
+    console.log("Model Loaded!");
+}
+
+function check()
+{
+    img=document.getElementById("captured_img");
+    classifier.classify(img,gotResult);
+}
+
+function gotResult(error,results)
+{
+if(error)
+{console.error(error);}
+else{
+    console.log(results);
+    document.getElementById("result_object_name").innerHTML=results[0].label;
+    document.getElementById("result_object_accuracy").innerHTML=results[0].confidence.toFixed(2);
+}
+}
+
